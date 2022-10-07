bb145c2c863e2e4f62c97a50ee500e14dbdbd466
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-10-08 01:22:42 +0300

    add start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
-export default function App(){
+export default function App() {
   return (
-<>
-  hello
-</>
+    <>
+      hello
+      <Header />
+    </>
   );
-};
-
+}
