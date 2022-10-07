c76e2dfa094d6612a3251e951155178f3d8cb003
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-10-07 23:11:38 +0300

    Update theme.js

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,8 +1,47 @@
 export const theme = {
   colors: {
-    black: '#000',
-    white: '#fff',
+    white: '#ffffff',
+    black: '#000000',
+
+    accent: '#FC842D',
+    primaryBackground: '',
+    secondaryBackground: '#264061',
+
+    text: {
+      sectionTitle: '#212121',
+      primaryText: '9B9FAA',
+      secondaryText: '#212121',
+      accent: '#FC842D',
+    },
+
+    button: {
+      primaryBtnBackground: '#FC842D',
+      secondaryBtnBackground: '#FFFFFF',
+      accentBtnBackground: '#2ee59d',
+      inputBorder: '#FC842D',
+      btnShadow: '#2ee59d66',
+      primaryText: 'FC842D',
+      secondaryText: '#FFFFFF',
+
+      primaryBtn: '',
+    },
+
+    form: {
+      formBorder: '#E0E0E0',
+      formTitle: '#9B9FAA',
+      formText: '#9B9FAA',
+      formPlaceholder: '',
+      formInputBorder: '#E0E0E0',
+      formErrorMessage: '#FC842D',
+      checkboxText: '#9B9FAA',
+      checkboxAccentText: '#FC842D',
+      checkboxAccent: '#FC842D',
+      checkboxBorder: '#E0E0E0',
+    },
+
+    searchInput: '',
   },
+
   space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
   fonts: {
     body: 'system-ui, sans-serif',
