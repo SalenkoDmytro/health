cf8d49ec58a8e15e850e44167bb168b9926384ce
Volodymyr Shamrai <s.v.777.energy@gmail.com>
2022-10-08 19:37:19 +0300

    add some
    треба фетчити юзера бо не зберігає пісдя перезагрузки, над візуалом трішки пізніше займусь

--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUserName } from 'redux/auth/authSelectors';
+import { selectUser } from 'redux/user/userSelectors';
 
 function UserInfo() {
-  return (
-    <div>UserInfo</div>
-  );
+  const isLogin = useSelector(selectUser);
+  const userName = useSelector(selectUserName);
+  console.log(userName);
+  return <>{isLogin ? <p>{userName}</p> : <p>Noot logined</p>}</>;
 }
 export default UserInfo;
