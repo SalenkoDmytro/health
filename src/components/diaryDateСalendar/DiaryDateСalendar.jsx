97d741d164d19a3ff1fb1e7b926198c9c73bc86f
Wolbu <94459033+Wolbu@users.noreply.github.com>
2022-10-09 23:54:53 +0200

    added calendar to diary page

--- src/components/diaryDateСalendar/DiaryDateСalendar.jsx
+++ src/components/diaryDateСalendar/DiaryDateСalendar.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 function DiaryDateСalendar(props) {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const createDate = date => {
+    return { date: date.toISOString().slice(0, 10) };
+  };
+
   return (
-    <div>DiaryDateСalendar</div>
+    <DatePicker
+      selected={startDate}
+      dateFormat="dd.MM.yyyy"
+      onChange={date => setStartDate(date)}
+    />
   );
 }
 
--- src/page/diaryPage/DiaryPage.jsx
+++ src/page/diaryPage/DiaryPage.jsx
@@ -1,24 +1,31 @@
 import React, { useEffect } from 'react';
-// import DiaryProductsList from '../../components/diaryProductsList/DiaryProductsList';
-// import DailyCaloriesForm from '../../components/dailyCaloriesForm/DailyCaloriesForm';
-// import DiaryDateCalendar from '../../components/diaryDateCalendar/DiaryDateCalendar';
+import DiaryProductsList from '../../components/diaryProductsList/DiaryProductsList';
+import DailyCaloriesForm from '../../components/dailyCaloriesForm/DailyCaloriesForm';
+import DiaryDateСalendar from 'components/diaryDateСalendar/DiaryDateСalendar';
+import RightSideBar from 'components/rightSideBar/RightSideBar';
 import { useDispatch, useSelector } from 'react-redux';
 import { productSearch } from '../../redux/productSearch/productSearchOperations';
 import { selectProduct } from '../../redux/productSearch/productSearchSelectors';
+import { paddingLeft } from 'styled-system';
 
 function DiaryPage() {
   const product = useSelector(selectProduct);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(productSearch('хлеб'))
+    dispatch(productSearch('хлеб'));
   }, [dispatch]);
-  console.log('product',product);
+  console.log('product', product);
   return (
-    <>
-      {/* <DiaryDateСalendar />
-      <DailyCaloriesForm />
-      <DiaryProductsList /> */}
-    </>
+    <div style={{ display: 'flex' }}>
+      <div style={{ width: '60%' }}>
+        <DiaryDateСalendar />
+        <DailyCaloriesForm />
+        <DiaryProductsList />
+      </div>
+      <div style={{ width: '40%' }}>
+        <RightSideBar />
+      </div>
+    </div>
   );
 }
 
