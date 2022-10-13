b8b25abc666b8bd1e344b509d322446f4d0254e5
MaxKryzh <maximkryzhanovsky@gmail.com>
2022-10-13 14:31:04 +0300

    added photos

--- src/page/notFoundPage/NotFoundPage.styled.js
+++ src/page/notFoundPage/NotFoundPage.styled.js
@@ -1,10 +1,40 @@
 import styled from 'styled-components';
+import Box from 'components/common/box/Box';
+import { device } from 'utils/device';
+
 // import hunger from 'assets/images/notFound/hunger-404.webp';
 import healthy from 'assets/images/notFound/flat-lay-404.jpeg';
 
 export const Image = styled.div`
+  margin-bottom: -30px;
   background-image: url(${healthy});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+`;
+
+export const BoxStyled = styled(Box)`
+  @media (${device.mobile}) {
+    height: 100vh;
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    margin-left: auto;
+    margin-top: -30px;
+    padding-bottom: -30px;
+    font-size: ${p => p.theme.fontSizes.xl};
+  }
+  @media (${device.fablet}) {
+    margin-top: -100px;
+    font-size: ${p => p.theme.fontSizes.xl};
+  }
+  @media (${device.tablet}) {
+    font-size: ${p => p.theme.fontSizes.xxl};
+  }
+  @media (${device.desktop}) {
+    color: #010101;
+    padding-bottom: -30px;
+  }
 `;
